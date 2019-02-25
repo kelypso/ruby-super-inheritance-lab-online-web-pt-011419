67e288339c1bf861ccdacbba80dc45366ddb46ab
Kelsey Low <kklowlow@gmail.com>
2019-02-25 04:39:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,9 @@
 class Student 
-  puts "Hey there! I'm so excited to learn stuff."
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+  
+  def raise_hand
+    puts "Pick me!"
+  end
 end
